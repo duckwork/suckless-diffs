85ff6dfc82fefb2cb96a80c451971a1c0c3b3cd1
Case Duckworth <cased123@gmail.com>
2016-03-08 17:13:14 -0600

    First commit

---
+++ wm.h
@@ -0,0 +1,25 @@
+/* Suckless-wide config */
+
+#define KILLKEY              F10
+
+#define FONT                 "DejaVu Sans Mono:pixelsize=12"
+
+#define COLOR_FG             "#f8ecc2"
+#define COLOR_BG             "#584c46"
+
+#define COLOR_BLACK          "#211c1a"
+#define COLOR_RED            "#da825f"
+#define COLOR_GREEN          "#73c064"
+#define COLOR_YELLOW         "#d6b51f"
+#define COLOR_BLUE           "#5091b2"
+#define COLOR_MAGENTA        "#ce7198"
+#define COLOR_CYAN           "#8eb2a4"
+#define COLOR_GRAY1          "#7f7664"
+#define COLOR_GRAY2          "#a89c84"
+#define COLOR_RED_BRIGHT     "#f26730"
+#define COLOR_GREEN_BRIGHT   "#beeb34"
+#define COLOR_YELLOW_BRIGHT  "#e59606"
+#define COLOR_BLUE_BRIGHT    "#21aabd"
+#define COLOR_MAGENTA_BRIGHT "#f2488f"
+#define COLOR_CYAN_BRIGHT    "#8eedc7"
+#define COLOR_WHITE          "#f8ecc2"
