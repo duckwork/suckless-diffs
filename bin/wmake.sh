e279ec681a630f67a1be1d2a8dfd39bfdad3465d
Case Duckworth <cased123@gmail.com>
2016-04-27 18:34:38 -0500

    Change colorscheme

--- bin/wmake.sh
+++ bin/wmake.sh
@@ -6,7 +6,10 @@ for d in *; do
   if [[ -d "$d" ]]; then
     cd $d;
     make clean;
-    make && sudo make install;
+    make;
+    if [[ $? == 0 ]] && [[ "$1""" == "-i" ]]; then
+      sudo make install;
+    fi
     cd "$cwd";
   fi
 done
---
+++ nirvana.h
@@ -0,0 +1,29 @@
+/* Suckless-wide config */
+
+#define KILLKEY              F10
+
+/* #define FONT                 "monospace:size=10:antialias=true:autohint=true" */
+#define FONT                 "Hack:style=Regular:size=10"
+#define PFONT                "monospace:size=9"
+
+#define COLOR_BLACK          "#221e1f"
+#define COLOR_RED            "#ac3a0d"
+#define COLOR_GREEN          "#097647"
+#define COLOR_YELLOW         "#ac680d"
+#define COLOR_BLUE           "#356c96"
+#define COLOR_MAGENTA        "#840f3e"
+#define COLOR_CYAN           "#0f5153"
+#define COLOR_GRAY1          "#69513a"
+#define COLOR_GRAY2          "#816952"
+#define COLOR_RED_BRIGHT     "#e87648"
+#define COLOR_GREEN_BRIGHT   "#329f70"
+#define COLOR_YELLOW_BRIGHT  "#e8a448"
+#define COLOR_BLUE_BRIGHT    "#356c96"
+#define COLOR_MAGENTA_BRIGHT "#d63a79"
+#define COLOR_CYAN_BRIGHT    "#32898c"
+#define COLOR_WHITE          "#ceaa78"
+
+#define COLOR_FG             COLOR_WHITE
+#define COLOR_BG             COLOR_BLACK
+/* #define COLOR_FG             COLOR_BLACK */
+/* #define COLOR_BG             COLOR_WHITE */
